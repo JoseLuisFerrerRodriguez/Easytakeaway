0affdff660b0d49eccbefd51329a93e931456c18
Jose Luis Ferrer Rodriguez <joseluis.ferrer.rodriguez@gmail.com>
2024-02-23 12:04:42 +0100

    Inicio del proyecto

---
+++ src/main/java/com/proyecto/easytakeaway/EasytakeawayApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.easytakeaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasytakeawayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasytakeawayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/easytakeaway/EasytakeawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.easytakeaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasytakeawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
